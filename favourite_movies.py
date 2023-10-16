a96c56af9d8be932ae4eab34779e1f38ca5e768a
GoldFish-Codecool <99878203+GoldFish-Codecool@users.noreply.github.com>
2023-10-16 13:40:10 +0000

    add Gladiator

--- favourite_movies.py
+++ favourite_movies.py
@@ -1,8 +1,7 @@
 # Dictionary mapping participants' names to their favourite movies
 favourite_movies = {
     "Presenter": "Inception",
-    "Cucc": "Cuccos",
-    "G": "M"
+    "Gábor": "Gladiator"
 }
 
 
